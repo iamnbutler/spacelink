cf4956dbe9c2f7f28b5000100d127e254fa2557b
Nate Butler <iamnbutler@gmail.com>
2015-11-06 04:28:10 -0500

    it livesss

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,67 @@
 $(document).ready(function(){  
 
-// TODO: Size chart based on window size
+d3.tsv("data/nasa_19950801.tsv", function(error, data) { // Get data from TSV using D3
 
-// Set up chart
-var chart = $("#chart");
+	// Set up chart
+	var chart = $("#chart");
 
-// Set up nodes
+	// Set up nodes
 
-var nodes			= [[2,1,0,1], [2,4,0,0.4],  [3,3,0,0.2],  [3,4,1,1],  [3,6,0,0.5],  [4,1,0,0.8],  [4,2,0,1] ],
-	node            = [0,0,0,0], // nodeRow, nodeCol, nodeType, nodeTime
-	nodeSize		= 5; // in percent (5 = 5%)
+	var nodeSize		= 2; // in percent (5 = 5%)
 
-for (var i, i = 0; i < nodes.length; i++) { 
-	var $newNode = $('<div class="node node-' + i + '"></div>'),
-		nodeRow	 = parseInt(nodes[i][0]) * nodeSize,
-		nodeCol	 = parseInt(nodes[i][1]) * nodeSize,
-		nodeType = nodes[i][2],
-		nodeTime = nodes[i][3];
+	// for (var i, i = 0; i < nodes.length; i++) {
+	// 	var $newNode = $('<div id="node-' + i + '" class="node"></div>'),
+	// 		nodeRow	 = parseInt(nodes[i][0]) * nodeSize,
+	// 		nodeCol	 = parseInt(nodes[i][1]) * nodeSize,
+	// 		nodeType = nodes[i][2],
+	// 		nodeTime = nodes[i][3];
 
-	chart.append($newNode); // Append a new node
-	var thisNode = chart.children('div').last(); // Save node as a variable
-	thisNode.css({ 
-				top: nodeRow + '%', left: nodeCol + '%', // Position Node
-				width: nodeSize + '%', height: nodeSize + '%', // Set node size
-				opacity: nodeTime }) // Set node time 
-			.addClass('nodeType' + nodeType); // Set node type
-}
+	// 	// TODO: build this comparison number w variable
+	// 	if (nodeCol > 41 ) {
+	// 		nodeRow++;
+	// 	}
 
-});
+	// 	chart.append($newNode); // Append a new node
+	// 	var thisNode = $('#node-' + i); // Save node as a variable
+	// 	thisNode.css({ 
+	// 				top: nodeRow + '%', left: nodeCol + '%', // Position Node
+	// 				width: nodeSize + '%', height: nodeSize + '%', // Set node size
+	// 				opacity: nodeTime }) // Set node time 
+	// 			.addClass('nodeType' + nodeType); // Set node type
+	// }
+
+	var nodeRow = 0,
+		nodeCol = 0;
+
+	for (var i, i = 0; i < 1681; i++) {
+
+		// TODO: build this comparison number w variable
+		if (nodeCol > 41 ) {
+			nodeRow++;
+			nodeCol = 0;
+		}
+
+		var $newNode = $('<div id="node-' + i + '" class="node"></div>'),
+			row		 = nodeRow * nodeSize,
+			col		 = nodeCol * nodeSize,
+			// TODO: get request data and convert it to nodeType
+			// nodeType = nodes[i][2],
+			nodeBytes = (Math.max(data[i].bytes, 10000) / 1000000); // Constrain data to 10k bytes (10kb) and convert to 0 - 1 range
+
+		chart.append($newNode); // Append a new node
+		var thisNode = $('#node-' + i); // Save node as a variable !! Must come after element is appended !!
+		thisNode.css({ 
+			top: row + '%', left: col + '%', // Position Node
+			width: nodeSize + '%', height: nodeSize + '%', // Set node size
+			opacity: nodeBytes // Set node time 
+			}) 
+		//	.addClass('nodeType' + nodeType); // Set node type
+
+		nodeCol++;
+	};
+}); // End d3.tsv
+
+}); // End doc.ready
 
 $(document).resize(function(){ 
 	// TODO: Update on window resize

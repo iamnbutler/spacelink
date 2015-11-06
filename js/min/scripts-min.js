8637a6481a79ce093cf23973e7a44737c4abc138
Nate Butler <iamnbutler@gmail.com>
2015-11-06 03:35:04 -0500

    Set up chart node js

--- js/min/scripts-min.js
+++ js/min/scripts-min.js
@@ -1,1 +1,1 @@
-for(i=0;i<nodes.length;i++);
+$(document).ready(function(){for(var e=$("#chart"),d=[[2,1,0,1],[2,4,0,.4],[3,3,0,.2],[3,4,1,1],[3,6,0,.5],[4,1,0,.8],[4,2,0,1]],n=[0,0,0,0],t=5,a,a=0;a<d.length;a++){var i=$('<div class="node node-'+a+'"></div>'),o=parseInt(d[a][0])*t,r=parseInt(d[a][1])*t,s=d[a][2],c=d[a][3];e.append(i);var p=e.children("div").last();p.css({top:o+"%",left:r+"%",width:t+"%",height:t+"%",opacity:c}).addClass("nodeType"+s)}}),$(document).resize(function(){});
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,34 @@
-// Uncomment if using jQuery
-// $(document).ready(function(){  });
+$(document).ready(function(){  
 
-for (i = 0; i < nodes.length; i++) { 
-    // text += nodes[i] + "<br>";
+// TODO: Size chart based on window size
+
+// Set up chart
+var chart = $("#chart");
+
+// Set up nodes
+
+var nodes			= [[2,1,0,1], [2,4,0,0.4],  [3,3,0,0.2],  [3,4,1,1],  [3,6,0,0.5],  [4,1,0,0.8],  [4,2,0,1] ],
+	node            = [0,0,0,0], // nodeRow, nodeCol, nodeType, nodeTime
+	nodeSize		= 5; // in percent (5 = 5%)
+
+for (var i, i = 0; i < nodes.length; i++) { 
+	var $newNode = $('<div class="node node-' + i + '"></div>'),
+		nodeRow	 = parseInt(nodes[i][0]) * nodeSize,
+		nodeCol	 = parseInt(nodes[i][1]) * nodeSize,
+		nodeType = nodes[i][2],
+		nodeTime = nodes[i][3];
+
+	chart.append($newNode); // Append a new node
+	var thisNode = chart.children('div').last(); // Save node as a variable
+	thisNode.css({ 
+				top: nodeRow + '%', left: nodeCol + '%', // Position Node
+				width: nodeSize + '%', height: nodeSize + '%', // Set node size
+				opacity: nodeTime }) // Set node time 
+			.addClass('nodeType' + nodeType); // Set node type
 }
+
+});
+
+$(document).resize(function(){ 
+	// TODO: Update on window resize
+});
